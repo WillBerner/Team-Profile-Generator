55b2781383279b435986b89e9aa3d9d961f72127
Will Berner <will.o.berner@gmail.com>
2021-08-07 20:40:46 -0400

    Added initial prompting functionality with question data.

---
+++ src/questions.js
@@ -0,0 +1,24 @@
+const questions = [
+    {
+        name: "managerName",
+        type: "input",
+        message: "Please enter the name of the team manager."
+    },
+    {
+        name: "managerID",
+        type: "input",
+        message: "Please enter the employee ID of the team manager."
+    },
+    {
+        name: "managerEmail",
+        type: "input",
+        message: "Please enter the email of the team manager."
+    },
+    {
+        name: "managerOfficeNumber",
+        type: "input",
+        message: "Please enter the office number of the team manager."
+    },
+]
+
+module.exports = questions;
