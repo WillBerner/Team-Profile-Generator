8afb69577d0573e883d11e643a840c539bc40f3d
Will Berner <will.o.berner@gmail.com>
2021-08-16 15:01:58 -0400

    Added comments to index.js.

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
+// Importing Inquirer for user input and fs to write the html file.
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+// Importing question arrays to use with inquirer
 const managerQuestions = require('./src/managerQuestions.js');
 const engineerQuestions = require('./src/engineerQuestions.js');
 const internQuestions = require('./src/internQuestions.js');
 
+// Importing necessary classes to represent team members
 let Manager = require('./lib/Manager.js');
 let Intern = require('./lib/Intern.js')
 let Engineer = require('./lib/Engineer.js')
 
-// WHEN I start the application
-// THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
-// WHEN I enter the team manager’s name, employee ID, email address, and office number
-// THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
-// WHEN I select the engineer option
-// THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
-// WHEN I select the intern option
-// THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
-// WHEN I decide to finish building my team
-// THEN I exit the application, and the HTML is generated
-
+// Generates html for a card to represent a specific team member
 function generateTeamMemberCard(teammember) {
 
+    // Will hold unique info html of team member (github/school/officeNumber)
     let uniqueInfo = '';
+
+    // Will hold html name and icon of team member
     let role = '';
 
+    // Fill vars based on team member's role
     switch (teammember.getRole()){
         case "Engineer":
             uniqueInfo = `GitHub: <a href="https://github.com/${teammember.getGithub()}">${teammember.getGithub()}</a>`;
@@ -41,6 +39,7 @@ function generateTeamMemberCard(teammember) {
             throw new Error("Sorry, team member role is not supported!")
     }
 
+    // Return an html snippet of a card with values based on the team member
     return `
 
     <div class="col s12 m6 l4">
@@ -60,12 +59,17 @@ function generateTeamMemberCard(teammember) {
 
     `;
 }
+
+// Generates general html structure for the resulting page
 function generateHTML(team) {
 
+    // Will be filled by all of the generated html cards for the team
     var teamHTML = '';
 
+    // Generate html for each team member and append them together to be inserted into the page
     team.forEach(teammember => teamHTML += generateTeamMemberCard(teammember));
 
+    // Return the generated html for the page with team member cards spliced in the middle
     return `
     <!DOCTYPE html>
     <html lang="en">
@@ -110,21 +114,28 @@ function generateHTML(team) {
     `;
 }
 
-
+// Writes the generated html to a new file: ./dist/index.html
 function writeHTML(data) {
 
+    // Get generated html from helper function using the user input data from inquirer
     let html = generateHTML(data);
 
+    // Write the file to distribution folder ./dist/index.html and log a success/failure
     fs.writeFile("./dist/index.html", html, (error) => {
         (error) ? log.error(error) : console.log("Success");
-    })
+    });
 }
 
+// Function to initialze the application and use inquirer to gather data.
 async function init() {
 
+    // Will hold results for a set of inquirer questions
     let results = null;
+
+    // Will hold the team member objects created
     let team = [];
 
+    // First, ask for the manager's information and add them to the team array
     await inquirer.prompt(managerQuestions)
 
         .then(data => results = data)
@@ -133,13 +144,16 @@ async function init() {
 
     team.push(new Manager(results.managerName, results.managerID, results.managerEmail, results.managerOfficeNumber));
 
+    // If the manager does not want to add any teammates, write the html file and return
     if (!results.addTeammate) {
         writeHTML(team);
         return;
     }
 
+    // While the manager wants to continue to add teammates, ask more questions
     while (results.addTeammate) {
 
+        // Determine whether to add an Engineer or an Intern and return that data
         let whoToAdd = await inquirer.prompt([{
             name: "result",
             type: "list",
@@ -147,6 +161,7 @@ async function init() {
             choices: ["Engineer", "Intern"]
         }]).then(data => data).catch((error) => console.error(error));
 
+        // If the manager wanted to add an engineer, prompt engineer questions and save the responses in the 'results' variable
         if (whoToAdd.result == "Engineer") {
 
             await inquirer.prompt(engineerQuestions)
@@ -155,8 +170,10 @@ async function init() {
 
                 .catch((error) => console.error(error));
 
+            // Add the new engineer to the team array
             team.push(new Engineer(results.engineerName, results.engineerID, results.engineerEmail, results.gitHub));
 
+        // If not adding an engineer, prompt intern questions and save the responses in the 'results' variable
         } else {
 
             await inquirer.prompt(internQuestions)
@@ -165,14 +182,17 @@ async function init() {
 
                 .catch((error) => console.error(error));
 
+            // Add the new Intern to the team array
             team.push(new Intern(results.internName, results.internID, results.internEmail, results.school));
 
         }
 
     }
 
+    // After the team creation is finished, generate and write an html file based on that team.
     writeHTML(team);
 
 }
 
+// Start her up!
 init();
