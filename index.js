87af8fa1665fd04251060097a657520e9eefdac1
Will Berner <will.o.berner@gmail.com>
2021-08-16 13:18:02 -0400

    Created basic card and layout for generated page.

--- index.js
+++ index.js
@@ -19,8 +19,46 @@ let Engineer = require('./lib/Engineer.js')
 // WHEN I decide to finish building my team
 // THEN I exit the application, and the HTML is generated
 
+function generateHTML(team) {
+
+    let teamString = '';
+    team.forEach(member => teamString += `Name: ${member.name}, Email: ${member.email}, Team ID: ${member.id}\n`)
+
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+        <title>Your Team</title>
+
+        <!-- Materialize CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+        <!-- Materialize JS -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+            
+        <!-- Custom CSS -->
+        <link rel="stylesheet" href="./style.css">
+
+    </head>
+    <body>
+
+    <h6>${teamString}<h6>
+    
+    </body>
+    </html>
+    `;
+}
+
+
 function writeHTML(data) {
-    fs.writeFile("./dist/result.html", data, (error) => {
+
+    let html = generateHTML(data);
+
+    fs.writeFile("./dist/result.html", html, (error) => {
         (error) ? log.error(error) : console.log("Success");
     })
 }
@@ -37,7 +75,7 @@ async function init() {
         .catch((error) => console.error(error));
 
     team.push(new Manager(results.managerName, results.managerID, results.managerEmail, results.managerOfficeNumber));
-    
+
     if (!results.addTeammate) {
         return;
     }
@@ -55,9 +93,9 @@ async function init() {
 
             await inquirer.prompt(engineerQuestions)
 
-            .then(data => results = data)
+                .then(data => results = data)
 
-            .catch((error) => console.error(error));
+                .catch((error) => console.error(error));
 
             team.push(new Engineer(results.engineerName, results.engineerID, results.engineerEmail, results.gitHub));
 
@@ -65,9 +103,9 @@ async function init() {
 
             await inquirer.prompt(internQuestions)
 
-            .then(data => results = data)
+                .then(data => results = data)
 
-            .catch((error) => console.error(error));
+                .catch((error) => console.error(error));
 
             team.push(new Intern(results.internName, results.internID, results.internEmail, results.school));
 
@@ -76,8 +114,7 @@ async function init() {
     }
 
     writeHTML(team);
-
-    console.log(team);
+    
 }
 
 init();
